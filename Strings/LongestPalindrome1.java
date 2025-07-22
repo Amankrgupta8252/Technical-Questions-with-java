04d0fb445ab3fc7169a6f4abe7ab7d5ba0693e57
Aman Gupta <kramangupta8552@gmail.com>
2025-07-22 21:24:36 +0530

    Initial commit

--- Strings/LongestPalindrome1.java
+++ Strings/LongestPalindrome1.java
@@ -5,7 +5,7 @@
 
  */
 
-import java.util.*;
+import java.util.Scanner;
 public class LongestPalindrome1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
