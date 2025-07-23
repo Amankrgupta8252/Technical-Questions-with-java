3998db5eb166622c0d6934123ce493918d7989a0
Aman Gupta <kramangupta8552@gmail.com>
2025-07-23 22:12:02 +0530

    firstNonRepeatingCharacter

---
+++ Strings/NonRepetingChar.java
@@ -0,0 +1,38 @@
+/*
+ 
+🤯 8. First Non-Repeating Character
+Input: "aabbcddbe"
+Output: "c"
+🧠 Jo character sirf ek baar aaya ho, sabse pehle.
+
+ */
+
+import java.util.*;
+public class NonRepetingChar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a String :");
+        String str = sc.nextLine();
+        char result = firstNonRepeatingCharacter(str);
+        System.out.println("First Non-Repeating Character: " + result);
+        sc.close();
+    }
+
+    public static char firstNonRepeatingCharacter(String str) {
+        
+        for (int i = 0; i < str.length(); i++) {
+            boolean isUnique = true;
+            for (int j = 0; j < str.length(); j++) {
+                if (i != j && str.charAt(i) == str.charAt(j)) {
+                    isUnique = false;
+                    break;
+                }
+            }
+
+            if (isUnique) {
+                return str.charAt(i);
+            }
+        }
+        return ' ';
+    }
+}
